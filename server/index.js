867e766109d23f8e080fff57e4931458c3acbea8
lequanglocdev <quanglocdev@gmail.com>
2024-05-29 16:15:41 +0700

    fix code

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require('express')
-const dbConnect = require("./config/dbConect");
+
 const  cookieParser = require("cookie-parser")
 const path = require("path")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const postRoute = require("./routes/post")
-const commentRoute = require("./routes/comment")
+const commentRoute = require("./routes/comment");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8888;
 app.use(cors());
 
-
+mongoose.connect('mongodb+srv://quanglocdev:peGlTUEhO51MTQ31@blog-web.bv8xqeg.mongodb.net/blog-web?retryWrites=true&w=majority&appName=blog-web')
 // đọc hiểu data mà client gửi lên
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
 app.use(express.json());
 
 app.use(cookieParser())
-dbConnect();
+
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
